d7f10ab8e1aac0afd1187a76e1e1a653c718a690
mkkiyoi <mkkiyoi@gmail.com>
2019-11-23 11:50:27 -0800

    Removed checked radial options for testing in survey.html

--- app/public/survey.js
+++ app/public/survey.js
@@ -1,5 +1,26 @@
-let addNewFriend = function(event) {
-    event.preventDefault();
+let validateForm = (name, imageURL, scores) => {
+    let valid = true;    
+    if (name === '') {
+        let errorDiv = $('div').addClass('alert alert-danger').text('Name is required.');
+        $('#name-group').append(errorDiv);
+        valid = false;
+        console.log("There was an error with name")
+    }
+    if (imageURL === '') {
+        let errorDiv = $('div').addClass('alert alert-danger').text('Valid URL to a photo is required.');
+        $('#photo-group').prepend(errorDiv);
+        valid = false;
+    }
+    if (scores.includes('')) {
+        let errorDiv = $('div').addClass('alert alert-danger').text('All survey questions must be answered.');
+        $('#survey-group').prepend(errorDiv);
+        valid = false;
+    }
+    return valid;
+}
+
+let addNewFriend = function() {
+    console.log("something")
     let name = $('#name').val().trim();
     let imageURL = $('#imageURL').val().trim();
     let scores = [];
@@ -7,29 +28,29 @@ let addNewFriend = function(event) {
         let answer = $("input[name='questionOptions" + i + "']:checked").val();
         scores.push(answer);
     }
-    let newFriend = {
-        name: name,
-        photo: imageURL,
-        scores: scores
+    let valid = validateForm(name, imageURL, scores);
+    if (valid) {
+        let newFriend = {
+            name: name,
+            photo: imageURL,
+            scores: scores
+        }
+        $.post('/api/friends', newFriend).then((data) => {
+            $('#matchName').html(data.name);
+            $('#matchPicture').attr('src', data.photo);
+        });
+        $('#matchModal').modal('show');
     }
-    
-    $.post('/api/friends', newFriend).then((data) => {
-        $('#matchName').html(data.name);
-        $('#matchPicture').attr('src', data.photo);
-    });
-}
-
-let getMatch = function(event) {
-    event.preventDefault();
-    $.get('/api/friends', (data) => {
-        console.log(data)
-    });
+    return valid;
 }
 
 $(document).ready(function() {
-    $('#submit').on('click', ((event) => {
-        addNewFriend(event);
-        // getMatch();
-        $('#matchModal').modal('show');
-    }));
+    $('#submit').on('submit', (event) => {
+        event.preventDefault();
+        return false;
+    });
+    $('#submit').on('click', (event) => {
+        event.preventDefault();
+        return addNewFriend();
+    });
 });
